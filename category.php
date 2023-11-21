8316bbde080d0ad6fdfc3cb612c62b9e0e3622f8
ChickenSoupe <93108270+ChickenSoupe@users.noreply.github.com>
2023-11-21 06:52:07 +0000

    Add files via upload

--- category.php
+++ category.php
@@ -1,4 +1,22 @@
 <?php include 'includes/session.php'; ?>
+<?php
+	$slug = $_GET['category'];
+
+	$conn = $pdo->open();
+
+	try{
+		$stmt = $conn->prepare("SELECT * FROM category WHERE cat_slug = :slug");
+		$stmt->execute(['slug' => $slug]);
+		$cat = $stmt->fetch();
+		$catid = $cat['id'];
+	}
+	catch(PDOException $e){
+		echo "There is some problem in connection: " . $e->getMessage();
+	}
+
+	$pdo->close();
+
+?>
 <?php include 'includes/header.php'; ?>
 <body class="hold-transition skin-purple layout-top-nav">
 <div class="wrapper">
@@ -8,45 +26,19 @@
 	  <div class="content-wrapper">
 	    <div class="container">
 
-	      <!-- Main content -->
+	      
 	      <section class="content">
 	        <div class="row">
 	        	<div class="col-sm-9">
-	        		<?php
-	        			if(isset($_SESSION['error'])){
-	        				echo "
-	        					<div class='alert alert-danger'>
-	        						".$_SESSION['error']."
-	        					</div>
-	        				";
-	        				unset($_SESSION['error']);
-	        			}
-	        		?>
-	        		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-		                <ol class="carousel-indicators">
-		                  <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-		                  <li data-target="#carousel-example-generic" data-slide-to="1" class=""></li>
-		                  <li data-target="#carousel-example-generic" data-slide-to="2" class=""></li>
-		                </ol>
-		                
-		                  </div>
-		                </div>
-		                <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-		                  <span class="fa fa-angle-left"></span>
-		                </a>
-		                <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-		                  <span class="fa fa-angle-right"></span>
-		                </a>
-		            </div>
-		            <h2>Welcome to Charlie's Fashion Emporium!</h2>
+		            <h1 class="page-header"><?php echo $cat['name']; ?></h1>
 		       		<?php
-		       			$month = date('m');
+		       			
 		       			$conn = $pdo->open();
 
 		       			try{
 		       			 	$inc = 3;	
-						    $stmt = $conn->prepare("SELECT *, SUM(quantity) AS total_qty FROM details LEFT JOIN sales ON sales.id=details.sales_id LEFT JOIN products ON products.id=details.product_id WHERE MONTH(sales_date) = '$month' GROUP BY details.product_id ORDER BY total_qty DESC LIMIT 6");
-						    $stmt->execute();
+						    $stmt = $conn->prepare("SELECT * FROM products WHERE category_id = :catid");
+						    $stmt->execute(['catid' => $catid]);
 						    foreach ($stmt as $row) {
 						    	$image = (!empty($row['photo'])) ? 'images/'.$row['photo'] : 'images/noimage.jpg';
 						    	$inc = ($inc == 3) ? 1 : $inc + 1;
@@ -56,8 +48,7 @@
 	       								<div class='box box-solid'>
 		       								<div class='box-body prod-body'>
 		       									<img src='".$image."' width='100%' height='230px' class='thumbnail'>
-												   <h5><a href='product.php?product=".$row['slug']."'>".$row['name']."</a></h5>
-												   <h5><a href='about.php
+		       									<h5><a href='product.php?product=".$row['slug']."'>".$row['name']."</a></h5>
 		       								</div>
 		       								<div class='box-footer'>
 		       									<b>&#36; ".number_format($row['price'], 2)."</b>
